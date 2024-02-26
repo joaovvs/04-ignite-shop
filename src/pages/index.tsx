c6744da9de6da1a9f5ba4bc6ffba965cc210c615
Joao Vinicius <joaovvs@gmail.com>
2024-02-26 18:39:35 -0300

    home layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,61 @@
-import { styled } from "../styles"
+import Image from "next/image"
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import { useKeenSlider } from 'keen-slider/react'
 
-  span: {
-    fontWeight: 'bold',
-  },
+import { HomeContainer, ProductContainer } from "../styles/pages/home"
 
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import camiseta1 from '../assets/1.png'
+import camiseta2 from '../assets/2.png'
+import camiseta3 from '../assets/3.png'
+import camiseta4 from '../assets/4.png'
 
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+    
+  })
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+
+      <ProductContainer className="keen-slider__slide"> 
+        <Image src={camiseta1} alt="" width={520} height={480}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </ProductContainer>
+      <ProductContainer className="keen-slider__slide"> 
+        <Image src={camiseta2} alt="" width={520} height={480}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </ProductContainer>
+      <ProductContainer className="keen-slider__slide"> 
+        <Image src={camiseta3} alt="" width={520} height={480}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </ProductContainer>
+      <ProductContainer className="keen-slider__slide"> 
+        <Image src={camiseta4} alt="" width={520} height={480}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </ProductContainer>
+
+
+    </HomeContainer>
   )
 }
