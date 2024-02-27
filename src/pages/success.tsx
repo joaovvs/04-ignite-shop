d3d28936d3a6c7944a49b653788b084110ea9a4f
Joao Vinicius <joaovvs@gmail.com>
2024-02-27 16:19:20 -0300

    success page with stripe session data

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,16 +1,29 @@
 import Link from "next/link";
-import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+import Image from "next/image"
 
-export default function Success(){
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+import { GetServerSideProps } from "next";
+import { stripe } from "../lib/stripe";
+import Stripe from "stripe";
+
+interface SuccessProps {
+    customerName: string
+        product: {
+        name: string
+        imageUrl: string
+    }
+}
+
+export default function Success({ customerName, product}: SuccessProps){
     return (
         <SuccessContainer>
             <h1>Compra efetuada!</h1>
-            <ImageContainer>
-
+            <ImageContainer >
+                <Image src={ product.imageUrl } alt="" width={120} height={110} priority={true}/>
             </ImageContainer>
 
             <p>
-                Uhuuul <strong>Usuário</strong>, sua <strong>camiseta</strong> já está a caminho da sua casa.
+                Uhuuul <strong>{ customerName }</strong>, sua <strong>{ product.name }</strong> já está a caminho da sua casa.
             </p>
 
 
@@ -19,4 +32,27 @@ export default function Success(){
             </Link>
         </SuccessContainer>
     )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    const sessionId = String(query.session_id)
+
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+        expand: ['line_items', 'line_items.data.price.product']
+    })
+
+    const customerName = session.customer_details?.name
+
+    const product = session.line_items?.data[0].price?.product as Stripe.Product
+    
+    return {
+        props: {
+            customerName,
+            product: {
+               name: product.name,
+               imageUrl: product.images[0],
+            }
+        }
+    }
 }
